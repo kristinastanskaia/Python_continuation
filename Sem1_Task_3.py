2ead536f6eb1b7ceea028a90bfa742f8484c8583
KristinaStanskaia <kristina.stanskaia@gmail.com>
2023-09-29 15:26:54 +0300

    Add the new solution

--- Sem1_Task_3.py
+++ Sem1_Task_3.py
@@ -3,18 +3,25 @@
 # если делится нацело только на единицу и на себя”. Сделайте ограничение на
 # ввод отрицательных чисел и чисел больше 100 тысяч.
 
-# a = int(input("Введите число: "))
-# print("составное")
-# проверить 0 и 1
+# num = int(input("Введите число: "))
+# number_description = "простое"
+# if 1 < num <= 100000:
+#     for i in range(2, num):
+#         if num % i > 0:
+#             continue
+#         else:
+#             number_description = "составное"
+#     print(number_description)
+# else:
+#     print("Число не подходит!")
 
 num = int(input("Введите число: "))
 number_description = "простое"
 if 1 < num <= 100000:
     for i in range(2, num):
-        if num % i > 0:
-            continue
-        else:
+        if num % i == 0:
             number_description = "составное"
+            break
     print(number_description)
 else:
     print("Число не подходит!")
