001e2393c32d9f6811996f563d8a9890933f4c55
KristinaStanskaia <kristina.stanskaia@gmail.com>
2023-10-04 14:23:57 +0300

    Add the end solution

--- Sem2b_Task_3.py
+++ Sem2b_Task_3.py
@@ -3,29 +3,44 @@
 # дробей. Для проверки своего кода используйте модуль fractions.
 
 import math
+import fractions
 
-num_1 = input("Введите числитель и знаменатель первого числа “a/b”: ")
-num_2 = input("Введите числитель и знаменатель первого числа “a/b”: ")
+fraction_1 = input("Введите числитель и знаменатель первого числа “a/b”: ")
+fraction_2 = input("Введите числитель и знаменатель второго числа “a/b”: ")
 
 # list_1 = a.split("/")
-list_1 = [int(x) for x in num_1.split("/")]
-list_2 = [int(x) for x in num_2.split("/")]
+list_1 = [int(x) for x in fraction_1.split("/")]
+num_1 = list_1[0]
+den_1 = list_1[1]
+
+list_2 = [int(x) for x in fraction_2.split("/")]
+num_2 = list_2[0]
+den_2 = list_2[1]
 
 # умножение
-multiplication_numerator = list_1[0] * list_2[0]
-multiplication_denominator = list_1[1] * list_2[1]
+multiplication_numerator = num_1 * num_2
+multiplication_denominator = den_1 * den_2
 
 # сложение
-if list_1[1] == list_2[1]:
-    addition_numerator = list_1[0] + list_2[0]
-    addition_denominator = list_1[1]
+if den_1 == den_2:
+    addition_numerator = num_1 + num_2
+    addition_denominator = den_1
 else:
-    addition_denominator = (list_1[1] * list_2[1]) // (
-        math.gcd(list_1[1], list_2[1]))
-    addition_numerator = (list_1[0] * (
-            addition_denominator // list_1[1])) + (list_2[0] * (
-            addition_denominator // list_2[1]))
+    addition_denominator = (den_1 * den_2) // (
+        math.gcd(den_1, den_2))
+    addition_numerator = (num_1 * (
+            addition_denominator // den_1)) + (num_2 * (
+            addition_denominator // den_2))
+
+# сокращение дробей
+nod_1 = math.gcd(multiplication_numerator, multiplication_denominator)
+nod_2 = math.gcd(addition_numerator, addition_denominator)
 
 print(
-    f"ПРОИЗВЕДЕНИЕ: {multiplication_numerator}/{multiplication_denominator}")
-print(f"CУММА: {addition_numerator}/{addition_denominator}")
+    f"ПРОИЗВЕДЕНИЕ: {multiplication_numerator // nod_1}/{multiplication_denominator // nod_1}")
+print(f"CУММА: {addition_numerator // nod_2}/{addition_denominator // nod_2}")
+print("Проверка: ")
+print((fractions.Fraction(num_1, den_1)) * (
+    fractions.Fraction(num_2, den_2)))
+print((fractions.Fraction(num_1, den_1)) + (
+    fractions.Fraction(num_2, den_2)))
